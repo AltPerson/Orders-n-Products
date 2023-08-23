29c375870140604e6c6cbc7c495343c10d65c03f
AltPerson <nothinghardforyou@gmail.com>
2023-08-23 18:46:50 +0300

    init

---
+++ src/App/App.tsx
@@ -0,0 +1,5 @@
+import './styles/index.scss';
+
+const App = () => <div className="app">app</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ReactRefreshTypeScript = require('react-refresh-typescript');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (_, argv) => {
+  const isDevelopment = argv === 'development';
+
+  return {
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      assetModuleFilename: 'assets/[name]__[hash][ext]',
+      clean: true,
+      publicPath: '/',
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+      runtimeChunk: 'single',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      preferAbsolute: true,
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      mainFiles: ['index'],
+      alias: {},
+    },
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          loader: 'ts-loader',
+          options: {
+            getCustomTransformers: () => ({
+              before: [isDevelopment && ReactRefreshTypeScript()].filter(
+                Boolean,
+              ),
+            }),
+          },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?[ac]ss$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  auto: (resPath) => Boolean(resPath.includes('.module.')),
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                },
+                importLoaders: 1,
+                sourceMap: true,
+              },
+            },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico|webp)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+      new webpack.ProgressPlugin(),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, 'public'),
+            to: path.join(__dirname, 'build'),
+            globOptions: {
+              ignore: ['**/index.html'],
+            },
+            noErrorOnMissing: true,
+          },
+        ],
+      }),
+      new webpack.DefinePlugin({
+        'process.env.PUBLIC_URL': JSON.stringify(
+          'http://localhost:3000/public',
+        ),
+      }),
+    ].filter(Boolean),
+    devServer: {
+      port: 3000,
+      open: true,
+      historyApiFallback: true,
+      client: {
+        overlay: {
+          warnings: false,
+        },
+      },
+    },
+  };
+};
